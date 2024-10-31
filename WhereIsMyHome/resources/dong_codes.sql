12fbfe6399a33fa5be3faec82d9e4fafeaab826d
정현수 <conjugate0519@gmail.com>
2024-10-31 17:06:57 +0900

    :card_file_box: Chore: 데이터베이스 변경
    Former-commit-id: 50940c76bbf6375509a5339b3c759d6f849bbe03

--- WhereIsMyHome/resources/dong_codes.sql
+++ WhereIsMyHome/resources/dong_codes.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f16733c1a0f359568d449fcaa80c09abb41c12ace224854fbf0fb80e79248b5
-size 1492
+oid sha256:1204f55afe4c6ea78b348cc0e17387dba2ae64c384040de8fcf4e41eabc8eb30
+size 1228
