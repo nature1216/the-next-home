8c8afb52a258bb517a6b2a2ed5dc7305b721d4b5
정현수 <conjugate0519@gmail.com>
2024-10-31 17:04:19 +0900

    :card_file_box: Chore: 데이터베이스 변경
    Former-commit-id: 164371e1dfcffdabe0629c9c64097005e70bb3db

--- WhereIsMyHome/resources/dong_codes.sql
+++ WhereIsMyHome/resources/dong_codes.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f6fbe07a7e4b5ca1e36a349a6fde450ff703cf19624b3c937ff42f6e8ebd1ef3
-size 1489
+oid sha256:5f16733c1a0f359568d449fcaa80c09abb41c12ace224854fbf0fb80e79248b5
+size 1492
--- WhereIsMyHome/resources/favorite_areas.sql
+++ WhereIsMyHome/resources/favorite_areas.sql
@@ -1,8 +1,8 @@
-create table favorite_areas (
-		favorite_area_id int primary key auto_increment,
-        user_info_id int,
-        sido_name varchar(45) not null,
-        gungu_name varchar(45),
-        dong_name varchar(45),
-        foreign key(user_info_id) references user_infos(user_info_id)
+DROP TABLE IF EXISTS `ssafyhome`.`members` ;
+
+create table members(
+	id varchar(45) primary key,
+    password varchar(128) not null,
+    email varchar(45) not null unique,
+    name varchar(45)
 );
