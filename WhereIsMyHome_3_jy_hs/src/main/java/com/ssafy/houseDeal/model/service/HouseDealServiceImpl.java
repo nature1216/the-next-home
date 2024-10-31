83c7d718bb9952bc17e9c7c8140d3edc6675600b
nature1216 <annekim1216@naver.com>
2024-11-01 01:12:36 +0900

    :sparkles: Feat: houseDeal spring boot rest controller 적용
    Former-commit-id: c06d5114dad14bd5168d24951cb3b78529afd144

---
+++ WhereIsMyHome_3_jy_hs/src/main/java/com/ssafy/houseDeal/model/service/HouseDealServiceImpl.java
@@ -0,0 +1,23 @@
+package com.ssafy.houseDeal.model.service;
+
+import com.ssafy.houseDeal.model.HouseDealDto;
+import com.ssafy.houseDeal.model.mapper.HouseDealMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class HouseDealServiceImpl implements HouseDealService {
+
+    HouseDealMapper houseDealMapper;
+
+    public HouseDealServiceImpl(HouseDealMapper houseDealMapper) {
+        this.houseDealMapper = houseDealMapper;
+    }
+
+    @Override
+    public List<HouseDealDto> getHouseDeals(Map<String, String> request) {
+        return houseDealMapper.getHouseDeals(request);
+    }
+}
--- WhereIsMyHome_3_jy_hs/src/main/java/com/ssafy/houseDeal/service/HouseDealServiceImpl.java
+++
@@ -1,5 +0,0 @@
-package com.ssafy.houseDeal.service;
-
-public class HouseDealServiceImpl implements HouseDealService {
-
-}
