09734500ffbbd8c458b71ed692dd75daf9ede74c
김자연 <annekim1216@naver.com>
2024-11-01 14:30:35 +0900

    :heavy_plus_sign: Chore: lombok 적용
    Former-commit-id: 9699ffc93353cacdc2149cdf025d645cf3f996e9

--- WhereIsMyHome_3_jy_hs/src/main/java/com/ssafy/favoriteArea/model/FavoriteAreaDto.java
+++ WhereIsMyHome_3_jy_hs/src/main/java/com/ssafy/favoriteArea/model/FavoriteAreaDto.java
@@ -1,31 +1,10 @@
 package com.ssafy.favoriteArea.model;
 
+import lombok.Data;
+
+@Data
 public class FavoriteAreaDto {
     private int id;
     private String memberId;
     private String dongCode;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getMemberId() {
-        return memberId;
-    }
-
-    public void setMemberId(String memberId) {
-        this.memberId = memberId;
-    }
-
-    public String getDongCode() {
-        return dongCode;
-    }
-
-    public void setDongCode(String dongCode) {
-        this.dongCode = dongCode;
-    }
 }
