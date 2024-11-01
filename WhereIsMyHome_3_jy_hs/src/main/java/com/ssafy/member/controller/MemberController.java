3f63b8f328002c75702a8422aee0a884b59adb65
정현수 <conjugate0519@gmail.com>
2024-11-01 17:33:31 +0900

    :poop: Feat: JWT 추가
    Former-commit-id: 65636a2bd0c78ccb15c933e2be9bbd6e50edd5b4

--- WhereIsMyHome_3_jy_hs/src/main/java/com/ssafy/member/controller/MemberController.java
+++ WhereIsMyHome_3_jy_hs/src/main/java/com/ssafy/member/controller/MemberController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.member.model.MemberDto;
 import com.ssafy.member.model.service.MemberService;
+import com.ssafy.security.JwtTokenProvider;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -31,25 +32,29 @@ import lombok.AllArgsConstructor;
 @Tag(name = "Member", description = "회원 관련 API")
 public class MemberController {
 	private final MemberService memberService;
+	private final JwtTokenProvider jwtTokenProvider;
 
 	@Operation(summary = "로그인", description = "사용자 로그인 처리")
 	@ApiResponses({ @ApiResponse(responseCode = "200", description = "로그인 성공"),
 			@ApiResponse(responseCode = "401", description = "로그인 실패 - 인증되지 않은 사용자"),
 			@ApiResponse(responseCode = "500", description = "서버 오류") })
 	@PostMapping("/login")
-	public ResponseEntity<MemberDto> login(@RequestBody Map<String, String> request) {
+	public ResponseEntity<String> login(@RequestBody Map<String, String> request) {
 		try {
 			MemberDto memberDto = memberService.login(request);
+			System.out.println(memberDto);
 			if (memberDto != null) {
 				// String token = jwtService.generateToken(member); // 로그인 성공 시 토큰 발급 : 추후 추가
 				// return ResponseEntity.ok().header("Authorization", "Bearer " +
 				// token).body("로그인 성공");
-				return ResponseEntity.ok(memberDto);
+				String token = jwtTokenProvider.createToken(memberDto);
+				System.out.println("토큰은"+ token);
+				return ResponseEntity.ok().header("Authorization", "Bearer " + token).body("로그인 성공");
 			} else {
-				return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+				return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인 실패");
 			}
 		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 오류");
 		}
 	}
 
