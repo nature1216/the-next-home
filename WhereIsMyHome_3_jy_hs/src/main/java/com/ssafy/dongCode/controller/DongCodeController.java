e5e43de88b340ea3aba5fb5a0f81f8696280975b
김자연 <annekim1216@naver.com>
2024-11-01 15:28:25 +0900

    :hammer: Docs: swagger 변경
    Former-commit-id: 37d61870736203744c68b2303f954357b8620569

--- WhereIsMyHome_3_jy_hs/src/main/java/com/ssafy/dongCode/controller/DongCodeController.java
+++ WhereIsMyHome_3_jy_hs/src/main/java/com/ssafy/dongCode/controller/DongCodeController.java
@@ -11,15 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ssafy.dongCode.model.DongCodeDto;
 import com.ssafy.dongCode.model.service.DongCodeService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/api/dongcode")
+@RequestMapping("/api/dong-code")
+@Tag(name = "Dong Code controller", description = "필터링 조건을 가져오는 DongCode API")
 public class DongCodeController {
 	
 	private final DongCodeService dongCodeService;
 	
+	@Operation(summary = "필터링 조건 가져오기", description = "상위 필터 선택 조건을 기반으로 해당 동 코드 필터 조건 목록을 가져옵니다.")
+	@Parameter(name="parentCode", description = "선택한 조건 코드", example="11")
 	@GetMapping
 	public ResponseEntity<List<DongCodeDto>> getFilters(@RequestParam String parentCode) {
 		List<DongCodeDto> list = dongCodeService.getFilters(parentCode);
