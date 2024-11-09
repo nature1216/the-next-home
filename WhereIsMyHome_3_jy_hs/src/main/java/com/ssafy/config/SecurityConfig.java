6eba61f66085137534979f888d63bbe3b8545204
hs20180519 <momo33559@naver.com>
2024-11-09 19:53:33 +0900

    :sparkles: Feat: Filter 및 SecurityConfig 추가
    Former-commit-id: d88c53caf11db6594ebd614e234471c326e55bd3

--- WhereIsMyHome_3_jy_hs/src/main/java/com/ssafy/config/SecurityConfig.java
+++ WhereIsMyHome_3_jy_hs/src/main/java/com/ssafy/config/SecurityConfig.java
@@ -1,45 +1,43 @@
 package com.ssafy.config;
 
-import com.ssafy.member.model.service.MemberService;
+import com.ssafy.security.JwtFilter;
 import com.ssafy.security.JwtTokenProvider;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-//        @Autowired
-//        private JwtTokenProvider jwtTokenProvider;
-//
-//        @Autowired
-//        private MemberService memberService;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private final JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable
-                )
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/member/login", "/api/member/join", "/css/**", "/js/**", "/images/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(AbstractHttpConfigurer::disable
-                )
+                .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(withDefaults())
                 .logout(logout -> logout
                         .logoutUrl("/api/member/logout")
                         .logoutSuccessUrl("/api/member/login?logout=true")
-                );
+                )
+                .addFilterBefore(jwtFilter,UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 }
-
