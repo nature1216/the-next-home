22b226f22014c1c6dbf71f7a9a0d3d087a088739
nature1216 <annekim1216@naver.com>
2024-11-10 23:48:08 +0900

    :sparkles: Feat: 회원가입시 email 인증 구현
    Former-commit-id: 79f2824512c3252222a3d7150d2bd904a591f372

--- WhereIsMyHome_3_jy_hs/src/main/java/com/ssafy/auth/controller/AuthController.java
+++ WhereIsMyHome_3_jy_hs/src/main/java/com/ssafy/auth/controller/AuthController.java
@@ -1,10 +1,29 @@
 package com.ssafy.auth.controller;
 
+import com.ssafy.auth.service.AuthService;
+import jakarta.mail.MessagingException;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/auth")
 public class AuthController {
+    private final AuthService authService;
 
+    @PostMapping("/join-mail")
+    public String sendMailForJoin(String mail, HttpSession session) throws MessagingException {
+        return authService.sendMailForJoin(mail, session) + "";
+    }
+
+    @PostMapping("/join-verification")
+    public ResponseEntity<Boolean> verifyCodeForJoin(@RequestParam("code")String code, HttpSession session) {
+        return ResponseEntity.ok(authService.verifyCodeForJoin(code, session));
+
+    }
 }
--- WhereIsMyHome_3_jy_hs/src/main/java/com/ssafy/auth/service/AuthService.java
+++ WhereIsMyHome_3_jy_hs/src/main/java/com/ssafy/auth/service/AuthService.java
@@ -1,8 +1,31 @@
 package com.ssafy.auth.service;
 
+import com.ssafy.util.MailSenderUtil;
+import jakarta.mail.MessagingException;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
+
+    MailSenderUtil mailSenderUtil;
+
+    private final String VERIfICATION_JOIN_CODE = "verificatioinJoinCode";
+
+    public AuthService(MailSenderUtil mailSenderUtil) {
+        this.mailSenderUtil = mailSenderUtil;
+    }
+    public int sendMailForJoin(String mail, HttpSession session) throws MessagingException {
+        int code = mailSenderUtil.sendMail(mail);
+        session.setAttribute(VERIfICATION_JOIN_CODE, code);
+        return code;
+    }
+
+    public boolean verifyCodeForJoin(String code, HttpSession session) {
+        String stored = (String) session.getAttribute(VERIfICATION_JOIN_CODE);
+
+        return stored != null && stored.equals(code);
+    }
 
 }
