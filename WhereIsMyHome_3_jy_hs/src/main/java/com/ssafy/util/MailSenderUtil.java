597d8a5fed6e75519d6f1116770be02ecffd86e2
nature1216 <annekim1216@naver.com>
2024-11-10 23:48:08 +0900

    :sparkles: Feat: 회원가입시 email 인증 구현
    Former-commit-id: 18973d7109006c132995995fc4a1c21a6201422d

--- WhereIsMyHome_3_jy_hs/src/main/java/com/ssafy/util/MailSenderUtil.java
+++ WhereIsMyHome_3_jy_hs/src/main/java/com/ssafy/util/MailSenderUtil.java
@@ -1,31 +1,33 @@
 package com.ssafy.util;
 
+import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
-import org.thymeleaf.templatemode.TemplateMode;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 
 @RequiredArgsConstructor
+@Component
 public class MailSenderUtil {
-	
+
 	private final JavaMailSender javaMailSender;
+	private final TemplateEngine templateEngine;
 	
 	@Value("${spring.mail.username}")
 	private String sender;
 	
-	private static int code;
-	
-	private static void createCode() {
-		code = (int) (Math.random() * (90000)) + 100000;
+	private int createCode() {
+		return (int) (Math.random() * (90000)) + 100000;
 	}
 	
-	public String createMail(String mail) {
-		createCode();
+	private String setContext(int code) {
 //		MimeMessage msg = javaMailSender.createMimeMessage();
 //		
 //		try {
@@ -42,24 +44,29 @@ public class MailSenderUtil {
 //		}
 		
 		Context context = new Context();
-		TemplateEngine templateEngine = new TemplateEngine();
-		ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
-		
+
 		context.setVariable("code", code);
 		
-		templateResolver.setPrefix("templates/");
-		templateResolver.setSuffix(".html");
-		templateResolver.setTemplateMode(TemplateMode.HTML);
-		templateResolver.setCacheable(false);
-		
-		templateEngine.setTemplateResolver(templateResolver);
-		
-		return templateEngine.process("mail", context);
+		return templateEngine.process("join", context);
+	}
+
+	private MimeMessage createEmailForm(String mail, int code) throws MessagingException {
+		MimeMessage message = javaMailSender.createMimeMessage();
+		message.addRecipients(MimeMessage.RecipientType.TO, mail);
+		message.setSubject("인증번호 전송 테스트 메일");
+		message.setFrom(sender);
+		message.setText(setContext(code), "utf-8", "html");
+
+		// TODO: Redis에 인증코드 설정
+
+		return message;
 	}
 	
-	public int sendMail(String mail) {
-		MimeMessage message = createMail(mail);
-		javaMailSender.send(message);
+	public int sendMail(String mail) throws MessagingException {
+		int code = createCode();
+		MimeMessage mailForm = createEmailForm(mail, code);
+
+		javaMailSender.send(mailForm);
 		
 		return code;
 	}
