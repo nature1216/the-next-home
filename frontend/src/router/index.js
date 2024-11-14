513cf08b0fb863856b759fc6d54f3ef688983351
김자연 <annekim1216@naver.com>
2024-11-14 17:59:34 +0900

    :sparkles: Feat: 매물 검색 페이지 구현 중
    Former-commit-id: 9103454b3eafd1854e3f88e53fb82322deccb244

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,31 +1,13 @@
 // router/index.js
+import HomePage from "@/views/HomePage.vue";
 import { createRouter, createWebHistory } from 'vue-router';
-import Login from '@/views/Login.vue';
-import SignUp from '@/views/SignUp.vue';
-import FindId from '@/views/FindId.vue';
-import Message from '@/views/Message.vue';
 
 const routes = [
   {
     path: '/',
     name: 'Login',
-    component: Login,
-  },
-  {
-    path: '/signup',
-    name: 'SignUp',
-    component: SignUp,
-  },
-  {
-    path: '/find-id',
-    name: 'FindId',
-    component: FindId,
-  },
-  {
-    path: '/message',
-    name: 'Message',
-    component: Message,
-  },
+    component: HomePage,
+  }
 ];
 
 const router = createRouter({
