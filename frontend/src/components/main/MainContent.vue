90c88a46956753146dcf8b8ebc4574cd3ecb0ec8
hs20180519 <momo33559@naver.com>
2024-11-14 23:12:53 +0900

    :sparkles: Feat: 로그인 페이지
    Former-commit-id: 39850ec4c531e7a9f89aff1a84f9ea56e2459339

---
+++ frontend/src/components/main/MainContent.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="main-content">
+    <div class="logo">The Next Home</div>
+    <h1>당신의 다음 집을 검색해보세요</h1>
+    <!--    <p>FIND YOUR NEXT HOME</p>-->
+    <SearchBox/>
+    <div class="auth-links">
+      <router-link to="/login">로그인</router-link>
+      /
+      <router-link to="/signup">회원가입</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import SearchBox from '@/components/common/SearchBox.vue';
+
+export default {
+  name: 'MainContent',
+  components: {
+    SearchBox,
+  },
+};
+</script>
+
+<style scoped>
+.main-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  flex: 1;
+}
+
+.logo {
+  font-size: 2em;
+  font-weight: bold;
+  color: #d87070;
+  letter-spacing: 2px;
+  text-transform: uppercase;
+}
+
+.auth-links {
+  margin-top: 1em;
+}
+
+.auth-links a {
+  color: #007bff;
+  text-decoration: none;
+}
+
+.auth-links a:hover {
+  text-decoration: underline;
+}
+</style>
---
+++ frontend/src/views/MemberLoginPage.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="login-page">
+    <member-login-form @login="handleLogin"/>
+    <div class="links">
+      <!--      <router-link to="/forgot-password-id">Forgot password / ID?</router-link>-->
+      <router-link to="/signup">아직 회원이 아니세요?</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import MemberLoginForm from '@/components/member/MemberLoginForm';
+
+export default {
+  components: {
+    MemberLoginForm
+  },
+  methods: {
+    handleLogin(credentials) {
+      console.log('로그인 시도:', credentials);
+      // 로그인 처리 로직을 여기에 추가
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* 로그인 페이지 스타일 */
+.login-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+}
+
+.links {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 15px;
+}
+</style>
+
