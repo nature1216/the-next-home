73228c88025d8208898946625768e198db67463d
정현수 <conjugate0519@gmail.com>
2024-11-15 14:27:23 +0900

    :card_file_box: Chore: 질문 게시판 db 추가
    Former-commit-id: 3f154b1cafb3b5ad61710177cb81c955c747cd75

--- backend/resources/members.sql
+++ backend/resources/members.sql
@@ -1,12 +1,10 @@
--- -----------------------------------------------------
--- Table `ssafyhome`.`favorite_areas`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `ssafyhome`.`favorite_areas` ;
+DROP TABLE IF EXISTS `ssafyhome`.`members` ;
 
-create table favorite_areas(
-	id int primary key auto_increment,
-    member_id varchar(45),
-    dong_code varchar(10),
-    foreign key(member_id) references members(id),
-    foreign key(dong_code) references dong_codes(dong_code)
+create table members(
+	id varchar(45) primary key,
+    password varchar(128) not null,
+    email varchar(45) not null unique,
+    role ENUM('user', 'admin') DEFAULT 'user',
+    name varchar(45),
+    
 );
