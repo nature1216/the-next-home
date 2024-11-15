a2c1c2f00d85c8b57da1951adeb11f2ca7e45514
정현수 <conjugate0519@gmail.com>
2024-11-15 15:10:00 +0900

    :sparkles: Feat: 질문/답변 CRUD
    Former-commit-id: f76ab5d1308c8df04c151fe5f59bd3d33394ca0b

---
+++ backend/src/main/java/com/ssafy/board/model/mapper/BoardMapper.java
@@ -0,0 +1,31 @@
+package com.ssafy.board.model.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.ssafy.board.model.BoardAnswerDto;
+import com.ssafy.board.model.BoardQuestionDto;
+
+@Mapper
+public interface BoardMapper {
+	// 질문 관련 메서드
+	List<BoardQuestionDto> getAllQuestions();
+
+	BoardQuestionDto getQuestionById(int id);
+
+	void createQuestion(BoardQuestionDto question);
+
+	void updateQuestion(BoardQuestionDto question);
+
+	void deleteQuestion(int id);
+
+	// 답변 관련 메서드
+	List<BoardAnswerDto> getAnswersByQuestionId(int questionId);
+
+	void createAnswer(BoardAnswerDto answer);
+
+	void updateAnswer(BoardAnswerDto answer);
+
+	void deleteAnswer(int id);
+}
