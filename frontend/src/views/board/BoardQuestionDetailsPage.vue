90658e1fb0680e4d2a54394a91fb68447b4c273a
정현수 <conjugate0519@gmail.com>
2024-11-15 17:54:05 +0900

    :sparkles: Feat: QnA 게시판 조회
    Former-commit-id: fd98587e39a41698ff1db4f4883e2203a179718f

---
+++ frontend/src/views/board/BoardQuestionDetailsPage.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="board-question-details-page">
+      <BoardQuestionDetails />
+    </div>
+  </template>
+  
+  <script>
+  import BoardQuestionDetails from '@/components/board/BoardQuestionDetails.vue';
+  
+  export default {
+    name: 'BoardQuestionDetailsPage',
+    components: {
+      BoardQuestionDetails,
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .board-question-details-page {
+    padding: 20px;
+  }
+  </style>
+  
