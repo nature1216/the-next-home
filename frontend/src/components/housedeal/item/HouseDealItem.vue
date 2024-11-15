681ebcc7032a897669d5dade211506db0e24d05b
김자연 <annekim1216@naver.com>
2024-11-15 13:35:09 +0900

    :sparkles: Feat: 매물 검색 front 구현 중
    Former-commit-id: 280fc75dbe6ba9d6394343f81b39560ee2f4df21

--- frontend/src/components/housedeal/item/HouseDealItem.vue
+++ frontend/src/components/housedeal/item/HouseDealItem.vue
@@ -1,10 +1,18 @@
 <script setup>
+import { defineProps, onUpdated } from 'vue'
 
+const props = defineProps({
+    item: Object
+})
+
+onUpdated(() => {
+    console.log(props.item);
+})
 </script>
 
 <template>
     <div>
-        
+        {{ item }}
     </div>
 </template>
 
--- frontend/src/views/HouseDealPage.vue
+++ frontend/src/views/HouseDealPage.vue
@@ -1,13 +1,69 @@
 <script setup>
 // import SideNavbar from '@/components/layout/SideNavbar.vue';
+import HouseDealList from "@/components/housedeal/HouseDealList.vue";
+import { ref, watch } from 'vue'
+import { getHomeDealByKeyword } from '@/api/houseDeal';
 import { onMounted } from 'vue';
+import { useRoute } from 'vue-router'
+
+const route = useRoute();
+
+const type = route.query.type;
+const keyword = route.query.keyword;
+
+const result = ref([]);
+
+watch(result, (newVal) => {
+    
+    result.value = newVal;
+    console.log(newVal)
+}, {
+    deep: true,
+    immediate: true
+})
+
+onMounted(() => {
+    console.log(type);
+    console.log(keyword);
+    getResultList(type, keyword);
+    console.log("왜안나와?????", result.value)
+})
+
+const getResultList = (type, keyword) => {
+    getHomeDealByKeyword(
+        {
+            type: type,
+            keyword: keyword
+        },
+        ({ data }) => {
+            result.value = data;
+            console.log(data);
+
+        },
+        (error) => {
+            console.log(error);
+        }
+    )
+}
+
+
+// const getResultList = async (type, keyword) => {
+//     try {
+//         const {data} = await getHomeDealByKeyword({ type, keyword });
+//         console.log(data);
+//         result.value = data;
+//     } catch (error) {
+//         console.error("데이터 로드 실패:", error);
+//     }
+// };
+
 
 
 </script>
 
 <template>
-    <!-- <SideNavbar /> -->
-
+    HouseDealPage
+    <HouseDealList :list='result' />
 </template>
 
 <style scoped>
