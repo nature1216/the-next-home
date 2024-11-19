d19a841317320b951a7729ed70f3eb5ac10ebfba
hs20180519 <momo33559@naver.com>
2024-11-19 21:41:12 +0900

    :sparkles: Feat: 로그아웃시 refresh token 삭제
    Former-commit-id: e3f2a47215d540f168c5e878c3bf1e9598d947a6

--- frontend/src/api/member.js
+++ frontend/src/api/member.js
@@ -1,7 +1,13 @@
 import {api} from "./Instance/axiosInstance";
+import {authApi} from "@/api/Instance/authInstance.js";
 
 const memberApi = api();
+const boardApi = authApi();
 
 export function login(body) {
   return memberApi.post("/auth/login", body);
+}
+
+export async function logout() {
+  return boardApi.post("/auth/logout")
 }
