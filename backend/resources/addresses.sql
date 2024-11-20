ffbbd39187a89db34fc4d6cbc95dd34bee348136
hs20180519 <momo33559@naver.com>
2024-11-21 06:17:52 +0900

    :card_file_box: Chore: addresses 테이블 수정
    Former-commit-id: 47cbd50bd8d169634838a20021db4e8e39f0babf

--- backend/resources/addresses.sql
+++ backend/resources/addresses.sql
@@ -3,9 +3,9 @@ DROP TABLE IF EXISTS `ssafyhome`.`addresses`;
 CREATE TABLE IF NOT EXISTS `ssafyhome`.`addresses`
 (
     addressId     INT AUTO_INCREMENT PRIMARY KEY,
-    memberId      INT NOT NULL,
+    memberId      VARCHAR(45) NOT NULL,
     zipcode       VARCHAR(10),
     jibunAddress  VARCHAR(255),
     detailAddress VARCHAR(255),
-    FOREIGN KEY (memberId) REFERENCES members (memberId)
+    FOREIGN KEY (memberId) REFERENCES members (id)
 );
