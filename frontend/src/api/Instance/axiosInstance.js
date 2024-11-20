7ad7183de83b69022ce2364fb8e4443717e31094
정현수 <conjugate0519@gmail.com>
2024-11-20 14:41:00 +0900

    :zap: session 유지 시간 jwt와 동일하게 설정
    Former-commit-id: bed431a7853b573e2c526a8e31cf0147f942155b

--- frontend/src/api/Instance/axiosInstance.js
+++ frontend/src/api/Instance/axiosInstance.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const { VITE_VUE_API_URL } = import.meta.env;
 
+// 로그인, 회원가입 + 인증 필요 없는 api
 function api() {
   const instance = axios.create({
     baseURL: VITE_VUE_API_URL,
