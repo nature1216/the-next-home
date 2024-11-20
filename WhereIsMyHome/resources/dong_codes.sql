f7aafb2a31bf87343d2e96683c0297c875914fc7
정현수 <conjugate0519@gmail.com>
2024-11-20 09:51:22 +0900

    :fire: Remove: WhereIsMyHome 초기 파일
    Former-commit-id: c690d210f9b29b7d6f9734ff47b669d6380cfb21

--- WhereIsMyHome/resources/dong_codes.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1204f55afe4c6ea78b348cc0e17387dba2ae64c384040de8fcf4e41eabc8eb30
-size 1228
--- WhereIsMyHome/resources/house_infos.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ccfe444578f9fa66b2cccef73fb8de9ae6929160a809d387d9e649840d3bfbcf
-size 917
