8e9d8c9b81e4a24c78062bda5133d65ec507a42a
hs20180519 <momo33559@naver.com>
2024-11-21 21:30:42 +0900

    :sparkles: Feat: 관심 매물 추가 추가, 조회, 삭제
    Former-commit-id: 37d87931d8519d58d9363ab999aaf0a1a26f068e

---
+++ backend/src/main/java/com/ssafy/favoriteProperty/model/service/FavoritePropertyServiceImpl.java
@@ -0,0 +1,32 @@
+package com.ssafy.favoriteProperty.model.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.ssafy.favoriteProperty.model.FavoritePropertyDto;
+import com.ssafy.favoriteProperty.model.mapper.FavoritePropertyMapper;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class FavoritePropertyServiceImpl implements FavoritePropertyService {
+
+	private final FavoritePropertyMapper favoritePropertyMapper;
+
+	@Override
+	public void addFavoriteProperty(FavoritePropertyDto favoritePropertyDto) {
+		favoritePropertyMapper.insertFavoriteProperty(favoritePropertyDto);
+	}
+
+	@Override
+	public List<FavoritePropertyDto> getFavoriteProperties(String memberId) {
+		return favoritePropertyMapper.selectFavoritePropertiesByMemberId(memberId);
+	}
+
+	@Override
+	public void deleteFavoriteProperty(String favoritePropertyId) {
+		favoritePropertyMapper.deleteFavoriteProperty(favoritePropertyId);
+	}
+}
