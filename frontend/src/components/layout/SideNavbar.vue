81a7da5adcf1ca961635e59dc0081a30d2e2d105
hs20180519 <momo33559@naver.com>
2024-11-21 21:59:50 +0900

    :lipstick: Design: star -> bookmark
    Former-commit-id: f82925d155424fbf190171416730c229f7ad9a75

--- frontend/src/components/layout/SideNavbar.vue
+++ frontend/src/components/layout/SideNavbar.vue
@@ -6,7 +6,7 @@
         <font-awesome-icon :icon="['fas', 'house']"/>
       </p>
       <p>
-        <font-awesome-icon :icon="['fas', 'star']"/>
+        <font-awesome-icon :icon="['fas', 'bookmark']"/>
       </p>
     </div>
 
@@ -16,14 +16,16 @@
       </div>
       <div>
         <p @click="goToBoard">
-     <font-awesome-icon :icon="['fas', 'circle-info']"/></p></div>
+          <font-awesome-icon :icon="['fas', 'circle-info']"/>
+        </p>
+      </div>
     </div>
   </div>
-  <DropdownMenu :showDropdown="showDropdown" />
+  <DropdownMenu :showDropdown="showDropdown"/>
 </template>
 
 <script>
-import { useAuthStore } from '@/stores/authStore'; // Pinia store 임포트
+import {useAuthStore} from '@/stores/authStore'; // Pinia store 임포트
 import DropdownMenu from '@/components/layout/DropDownMenu.vue'; // DropdownMenu 컴포넌트 임포트
 
 export default {
@@ -79,6 +81,7 @@ export default {
 
 .nav-links p {
   display: flex;
+  justify-content: center;
   align-items: center;
   margin: 10px 0;
   text-align: center;
