6fd1c4ec3ad690d1285114b490662ad55eabd3cf
hs20180519 <momo33559@naver.com>
2024-11-24 22:57:15 +0900

    :lipstick: Design: 찜 매물 UI 변경
    Former-commit-id: 4eaa78428394da987952cbd1b5b5b77316996be8

---
+++ backend/src/main/java/com/ssafy/favoriteProperty/model/DistanceInfoDto.java
@@ -0,0 +1,11 @@
+package com.ssafy.favoriteProperty.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class DistanceInfoDto {
+	private String addressName;
+	private String distance;
+}
