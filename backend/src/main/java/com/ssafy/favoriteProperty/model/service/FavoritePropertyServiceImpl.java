13d8cb7e31008f3a2d228a515b3c26bd2a69f7ea
hs20180519 <momo33559@naver.com>
2024-11-24 22:57:15 +0900

    :lipstick: Design: 찜 매물 UI 변경
    Former-commit-id: cd5d4afca7faaee93d3b61bd7582f91706c54d63

--- backend/src/main/java/com/ssafy/favoriteProperty/model/service/FavoritePropertyServiceImpl.java
+++ backend/src/main/java/com/ssafy/favoriteProperty/model/service/FavoritePropertyServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.ssafy.address.model.AddressDto;
 import com.ssafy.address.model.mapper.AddressMapper;
+import com.ssafy.favoriteProperty.model.DistanceInfoDto;
 import com.ssafy.favoriteProperty.model.FavoritePropertyDetailsDto;
 import com.ssafy.favoriteProperty.model.FavoritePropertyDto;
 import com.ssafy.favoriteProperty.model.mapper.FavoritePropertyMapper;
@@ -30,32 +31,35 @@ public class FavoritePropertyServiceImpl implements FavoritePropertyService {
 	@Override
 	public List<FavoritePropertyDetailsDto> getFavoriteProperties(String memberId) {
 		// 회원의 관심 매물 정보 가져오기
+		// 회원의 관심 매물 정보 가져오기
 		List<FavoritePropertyDetailsDto> properties = favoritePropertyMapper
 			.selectFavoritePropertiesByMemberId(memberId);
 
 		// 회원의 주소지 정보 가져오기
-		List<AddressDto> addresses = addressMapper.selectAddressesByMemberId(memberId); // 주소 정보 가져오기
+		List<AddressDto> addresses = addressMapper.selectAddressesByMemberId(memberId);
 
 		// 매물에 대한 거리 계산
 		for (FavoritePropertyDetailsDto property : properties) {
 			// 각 매물에 대해 거리 계산 (회원의 모든 주소에 대해)
-			List<String> distances = addresses.stream()
+			List<DistanceInfoDto> distances = addresses.stream()
 				.map(address -> {
 					try {
 						// 카카오 API를 통해 거리 계산
 						double distance = kakaoApiUtil.calculateDistance(address.getRoadNameAddress(),
 							property.getRoadAddress());
 
-						// 소수점 3자리에서 반올림
-						String formattedDistance = String.format("%.2f", distance);
+						// 소수점 2자리에서 반올림
+						String formattedDistance = String.format("%.2f km", distance);
 
-						return address.getName() + "까지 " + formattedDistance + "km"; // 거리 정보 추가
+						// 새로운 DTO에 저장
+						return new DistanceInfoDto(address.getName(), formattedDistance);
 					} catch (Exception e) {
 						e.printStackTrace(); // 예외 처리
-						return "거리 계산 실패"; // 예외 발생 시 대체 값
+						return new DistanceInfoDto(address.getName(), "거리 계산 실패");
 					}
 				})
 				.collect(Collectors.toList());
+
 			property.setDistances(distances); // DTO에 거리 정보 추가
 		}
 
@@ -64,9 +68,10 @@ public class FavoritePropertyServiceImpl implements FavoritePropertyService {
 
 	@Override
 	public boolean existsFavoritePropertyByAptSeqAndId(String memberId, String aptSeq) {
-		FavoritePropertyDto favoriteProperty = favoritePropertyMapper.getFavoritePropertyByAptSeqAndId(memberId, aptSeq);
+		FavoritePropertyDto favoriteProperty = favoritePropertyMapper.getFavoritePropertyByAptSeqAndId(memberId,
+			aptSeq);
 		System.out.println(favoriteProperty);
-		if(favoriteProperty == null) {
+		if (favoriteProperty == null) {
 			return false;
 		}
 		return true;
