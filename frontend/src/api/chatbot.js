613df224447f4e0fb658a6c0176c0b1095b77bb6
정현수 <conjugate0519@gmail.com>
2024-11-25 13:49:38 +0900

    :art: Refactor: 사용하지 않는 property 제거
    Former-commit-id: ca666d64e5dbea735d2b5809b05c6416b8e274e1

--- frontend/src/api/chatbot.js
+++ frontend/src/api/chatbot.js
@@ -2,10 +2,6 @@ import { authApi } from "./Instance/authInstance";
 
 const chatbotApi = authApi();
 
-export async function sendMessage(message, favoriteProperties, success, fail) {
-  // 서버로 전송할 데이터에 `message`와 `favoriteProperties` 추가
-  return chatbotApi
-    .post("/chat", { message, favoriteProperties }) // favoriteProperties도 함께 전달
-    .then(success)
-    .catch(fail);
+export async function sendMessage(message, success, fail) {
+  return chatbotApi.post("/chat", { message }).then(success).catch(fail);
 }
