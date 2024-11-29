c049ca635349938e0a5a8743b464b9a124d4657c
hs20180519 <momo33559@naver.com>
2024-11-29 10:52:58 +0900

    :fire: Remove: frontend git 파일
    Former-commit-id: 51dd377c243099546a7301e514f5da362896172e [formerly c65f6daa386d81306299776dbe450bd48fa7a8b2]
    Former-commit-id: a275d1f00297d6b2969a1f502d345da04f328be0

--- backend/resources/db/addresses.sql
+++ backend/resources/db/addresses.sql
@@ -1,10 +1,3 @@
-DROP TABLE IF EXISTS `ssafyhome`.`addresses`;
-
-CREATE TABLE IF NOT EXISTS `ssafyhome`.`addresses`
-(
-    address_id     INT AUTO_INCREMENT PRIMARY KEY,
-    member_id      VARCHAR(45) NOT NULL,
-    name		VARCHAR(20),
-   	road_name_address  VARCHAR(255),
-    FOREIGN KEY (member_id) REFERENCES members (id) ON DELETE CASCADE
-);
+version https://git-lfs.github.com/spec/v1
+oid sha256:8304fd4d1b066a7d3053172c5cf0249db9affec7e672a9d4841a58a1eff3a7e2
+size 324
